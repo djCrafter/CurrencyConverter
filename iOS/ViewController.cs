1dbe8e24676b13b9fa0bb40ff1117f11c6f4ccc8
Crafter <godhedin2000@gmail.com>
2019-05-28 21:38:50 +0300

    Second Commit

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using currencyconverter.AuthorizationModule;
+using System.Threading;
 
 using UIKit;
 
@@ -6,29 +8,54 @@ namespace currencyconverter.iOS
 {
     public partial class ViewController : UIViewController
     {
-        int count = 1;
+        const string TEST_LOGIN = "admin";
+        const string TEST_PASS = "12345";
 
         public ViewController(IntPtr handle) : base(handle)
         {
         }
 
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view, typically from a nib.
-            Button.AccessibilityIdentifier = "myButton";
-            Button.TouchUpInside += delegate
-            {
-                var title = string.Format("{0} clicks!", count++);
-                Button.SetTitle(title, UIControlState.Normal);
-            };
+        }
+
+        partial void LoginButton_TouchUpInside(UIButton sender)
+        {
+            var authSender = new AuthSender(TEST_LOGIN, TEST_PASS);
+            var interactor = new AuthorizationInteractor(new LoginValidator(), new PasswordValidator(), authSender);
+
+            var result = interactor.Login(loginField.Text, passwordField.Text).Result;
+
+            var message = GetMessage(result);
+            Alert(message);
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.		
         }
+
+
+        private string GetMessage(EAuthResult result)
+        {
+            switch (result)
+            {
+                case EAuthResult.InvalidData: return "Username or password is not entered!";
+                case EAuthResult.Success: return "Authenticated successfully!";
+                case EAuthResult.Unauthorized: return "Incorrect login or password!";
+                default: return "";
+            }
+        }
+
+        private void Alert(string message)
+        {
+            UIAlertController alert = UIAlertController.Create("", message, UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (actionOK) => { }));
+            this.PresentViewController(alert, true, null);
+        }
+
     }
 }
